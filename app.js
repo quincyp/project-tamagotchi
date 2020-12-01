e8e88a7787749d15daf4b3ec43399dc1d9a48015
qtpham <quincytpham@gmail.com>
2020-11-30 17:42:23 -0800

    Added initial outline checklist

---
+++ app.js
@@ -0,0 +1,29 @@
+/*
+MVP Minimum Viable Product Outline:
+    Show pet
+    Name
+        -Request a name
+        -Display name
+    Age
+        -start age at 0? 1?
+        - increase at X min
+    Stats
+        -Hunger
+        -Sleepiness
+        -Boredom
+            -- start at 1
+                --- if stat = 10 -> die
+            -- increase stat per X min
+    Buttons
+        - Feed [hunger]
+        - Turn off light [sleep]
+        - Play [bored]
+
+Version 2.0 Outline:
+    Style the page [more than initial]
+    Morph pet at certain age
+        - morphed stat interactions?
+    Animate live pet
+Stretch / Icebox Features:
+    to be decided
+*/
