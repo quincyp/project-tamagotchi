22a2a20928f542b6f798bb19366569e60f77c600
qtpham <quincytpham@gmail.com>
2020-12-01 16:56:42 -0800

    Added IDs for hunger bar and button, added event listener and functions, MVP button increases bar

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /*
 MVP Minimum Viable Product Outline:
-    Show pet
+    Show pet/
     Name
         -Request a name
         -Display name
@@ -30,3 +30,21 @@ Stretch / Icebox Features:
 
 console.log("Test");
 console.log($);
+
+// STEP ONE: ADD BUTTON LISTENER
+const $feedButton = $("#button--feed");
+const test = function test(event) {
+    console.log("test");
+}
+
+// const stat = 1;
+
+const $hungerBar = $("#bar--hunger");
+const updateStat = function updateStat(event) {
+    console.log($hungerBar);
+    $hungerBar.val($hungerBar.val() + 10);
+}
+
+/* *** CALLING SCRIPTS FOR TESTING *** */
+$feedButton.on("click", updateStat);
+// $playButton.on("click", test);
